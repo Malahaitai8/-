d4af50ed23665b63e562fa80a4e8925055adf499
DoyeonKing <23301153@bjtu.edu.cn>
2025-06-10 20:42:08 +0800

    参加更多队伍已经完成

--- volunteer-management-system/springboot/src/main/java/com/example/springboot/mapper/OrganizationMapper.java
+++ volunteer-management-system/springboot/src/main/java/com/example/springboot/mapper/OrganizationMapper.java
@@ -96,4 +96,53 @@ public interface OrganizationMapper {
      */
     @Delete("DELETE FROM tbl_Organization WHERE OrgID = #{orgId}")
     int deleteByOrgId(@Param("orgId") String orgId);
+
+           /**
+     * 【更新】查询某个志愿者可以加入的组织列表（即该志愿者尚未申请、尚未加入的组织）。
+     * 排除掉志愿者是“申请中”或“已加入”状态的组织，但会包含“已退出”的组织。
+     * 可以根据组织名称进行模糊查询。
+     *
+     * @param volunteerId 志愿者的ID，用于排除已关联的组织。
+     * @param orgName 组织名称的模糊查询关键词，如果为null或空，则不进行名称过滤。
+     * @return 符合条件的Organization列表。
+     */
+    @Select("<script>" +
+            "SELECT o.orgId, o.orgName, o.contactPersonPhone, o.serviceRegion, o.orgScale, " +
+            "o.orgRating, o.orgAccountStatus, o.totalServiceHours, o.activityCount, o.trainingCount " +
+            "FROM tbl_Organization o " +
+            "WHERE o.orgAccountStatus = '已认证' " + // 只显示已认证的组织
+            "AND NOT EXISTS ( " +
+            "   SELECT 1 " +
+            "   FROM tbl_VolunteerOrganizationJoin voj " +
+            "   WHERE voj.orgId = o.orgId " +
+            "     AND voj.volunteerId = #{volunteerId} " +
+            "     AND voj.memberStatus IN (N'申请中', N'已加入') " +
+            ") " +
+            "<if test='orgName != null and orgName != \"\"'>" +
+            "AND o.orgName LIKE CONCAT('%', #{orgName}, '%') " +
+            "</if>" +
+            "</script>")
+    List<Organization> findAvailableOrganizationsForVolunteer(@Param("volunteerId") String volunteerId,
+                                                              @Param("orgName") String orgName);
+    /**
+     * 统计某个志愿者可以加入的组织数量，用于分页。
+     *
+     * @param volunteerId 志愿者的ID，用于排除已关联的组织。
+     * @param orgName 组织名称的模糊查询关键词。
+     * @return 符合条件的组织总数。
+     */
+    @Select("<script>" +
+            "SELECT COUNT(o.orgId) " +
+            "FROM tbl_Organization o " +
+            "LEFT JOIN tbl_VolunteerOrganizationJoin voj ON o.orgId = voj.orgId AND voj.volunteerId = #{volunteerId} " +
+            "WHERE voj.volunteerId IS NULL " + // 排除已存在关联的组织
+            "AND o.orgAccountStatus = '已认证'" + // 只统计已认证的组织
+            "<if test='orgName != null and orgName != \"\"'>" +
+            "AND o.orgName LIKE CONCAT('%', #{orgName}, '%') " +
+            "</if>" +
+            "</script>")
+    int countAvailableOrganizationsForVolunteer(@Param("volunteerId") String volunteerId,
+                                                @Param("orgName") String orgName);
+
+
 }
